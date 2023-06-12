2bc1f2764dde567c0b4eb08e40faf19480df4849
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-06-12 20:33:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(200)
+        abort(400)
 
     return 'OK'
 
