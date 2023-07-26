879aa458f0b6607104c36db1afe3aa3497649317
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-26 14:47:56 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -5,5 +5,7 @@ user_status={}
 class MessageHandler:
     def reply(receivedEvent):
         text=event.message.text
+        # if event.source.type=='user':
+        #     user_status[event.source.userId]=
         answer=text
         return answer
