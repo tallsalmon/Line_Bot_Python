ed3d3d947b3737245fff67f4f769af278bd5769f
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-27 12:10:52 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -3,9 +3,9 @@ import json
 #user_status={}
 
 class MessageHandler:
-    def reply(receivedEvent):
+    def reply(receivedMessage):
         #text=event.message.text
         # if event.source.type=='user':
         #     user_status[event.source.userId]=
         #answer=text
-        return receivedEvent.message.text
+        return receivedMessage
