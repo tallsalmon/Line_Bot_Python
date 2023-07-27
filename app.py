6f1d2246bd3cb0b885a343be653d330631678e71
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-27 12:40:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    reply = MessageHandler.reply(event.message.text)
+    reply = MessageHandler.reply(event)
     line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply))
