a425ae88ce02d60e3f78f3b715e6f8d44551459d
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-27 12:47:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(event))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
