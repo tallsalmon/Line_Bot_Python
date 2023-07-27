0049481b16a6a205f53ff739b497972b36606ccf
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-27 22:05:45 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -1,12 +1,18 @@
 import json
 
-#user_status={}
+user_status={}
 
 class MessageHandler:
     def reply(receivedEvent):
         text=receivedEvent.message.text
-        # if event.source.type=='user':
-        #     user_status[event.source.userId]=
+        userId=event.source.userId
+        if event.source.type=='user':
+            if event.source.type not in user_status:
+                user_status[userId]=0
+
+            if user_status[userId]==0:
+                text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
+
         #answer=text
         #text='A'
         return text
