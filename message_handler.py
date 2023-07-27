44de6a226174386913f087e93e4c82b5d2feeca7
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-27 23:06:11 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -8,14 +8,15 @@ class MessageHandler:
         text=receivedEvent.message.text
 
         text=str(type(receivedEvent.source))
-        id=receivedEvent.source[userId]
-        if receivedEvent.source[type]=='user':
-            if id not in user_status:
-                user_status[id]=0
+        
+        # id=receivedEvent.source[userId]
+        # if receivedEvent.source[type]=='user':
+        #     if id not in user_status:
+        #         user_status[id]=0
 
-            if user_status[id]==0:
-                text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
-                user_status[id]+=1
+        #     if user_status[id]==0:
+        #         text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
+        #         user_status[id]+=1
                 
         #answer=text
         #text='A'
