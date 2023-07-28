b8bb85d7a9962b0b78df4464a162e5b0888fb3f0
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-07-28 22:58:15 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -5,22 +5,22 @@ user_status={}
 class MessageHandler:
     def reply(receivedEvent):
         #print(receivedEvent)
-        #text=receivedEvent.message.text
+        text=receivedEvent.message.text
         #text=str(receivedEvent.source['userId'])
         source=str(receivedEvent.source)
         r=source.rfind('"')
         l=source.rfind('"',0,r-1)
         id=source[l+1:r]
-        text=id
+        #text=id
         
-        # id=receivedEvent.source[userId]
-        # if receivedEvent.source[type]=='user':
-        #     if id not in user_status:
-        #         user_status[id]=0
+        #id=receivedEvent.source[userId]
+        if receivedEvent.source[type]=='user':
+            if id not in user_status:
+                user_status[id]=0
 
-        #     if user_status[id]==0:
-        #         text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
-        #         user_status[id]+=1
+            if user_status[id]==0:
+                text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
+                user_status[id]+=1
                 
         #answer=text
         #text='A'
