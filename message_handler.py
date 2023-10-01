5c7b3b27a8a702f50defa8afe3bb202b14c20c9d
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-10-01 22:27:18 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -4,7 +4,7 @@ import os
 import dropbox
 
 # Dropbox(Salmonfs)のアクセストークン
-DROPBOX_ACCESS_TOKEN = 'sl.BmitubrYv1ESE5vLWYOrJ7ZPHWQwAoc1sEYEd6fEFqEPOvBS0ccLKAQ_lRcuKZ-o-D-Ka3CMaoPRNeWqNLQoPmSWY0Dvq9mz8ngMtN59hzYIne3jiO2rXX9iuBQBaJXNg65KvtAotzyt'
+DROPBOX_ACCESS_TOKEN = 'sl.BnHRHWk2rXzCQdmN0qckxTISBxln8jxuAQw-2gllG4gjeLbgUU1D1gIavCoy0C9qXeLmm4XMKSnI3_jV1OIqJwew8c2N4fQiFwd9YASQEnDLforzgksimjxKSH0Kt9eJt6Doh6i54Ifp'
 # Dropboxのフォルダ
 # Dropboxのルートにこの名前のフォルダを事前に作っておく必要がある
 DROPBOX_ROOT = '/fujishima_weasel/'
@@ -42,13 +42,12 @@ class MessageHandler:
         # t = m.group()
         # userid = t[10:-2]
 
-        f = open('myfile.txt', 'w')
-        f.write(text)
-        f.close()
-        # アップロードしたいファイル
-        local_filepath = 'myfile.txt'
-        send_num[0]+=1
-
+        # f = open('myfile.txt', 'w')
+        # f.write(text)
+        # f.close()
+        # # アップロードしたいファイル
+        # local_filepath = 'myfile.txt'
+        # send_num[0]+=1
         # アップロード先のファイル名（アップロードしたいファイルと同じ名前でもよい）
         # dropbox_filepath = 'test'+str(send_num[0])+'.txt'
         # ファイルアップロード
@@ -61,7 +60,7 @@ class MessageHandler:
         if type=='user':
             if id not in user_status:
                 user_status[id]=0
-                user_status[id]=[]
+                user_answer[id]=[]
 
             if user_status[id]==0:
                 text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
@@ -78,17 +77,17 @@ class MessageHandler:
             elif user_status[id]==3:
                 text='捕獲した場所を教えて下さい。以下のようにお答えください。例：福井県〇〇市〇〇町〇〇番地'
                 user_status[id]=4
-                user_status[id].append(receivedEvent.message.text)#日時の記録
+                user_answer[id].append(receivedEvent.message.text)#日時の記録
 
             elif user_status[id]==4:
                 text='捕獲者のお名前を教えて下さい。'
                 user_status[id]=5
-                user_status[id].append(receivedEvent.message.text)#場所
+                user_answer[id].append(receivedEvent.message.text)#場所
 
             elif user_status[id]==5:
                 text='次に捕獲したイタチの状態について以下の３つの質問に答えてください。'
                 user_status[id]=6
-                user_status[id].append(receivedEvent.message.text)#名前
+                user_answer[id].append(receivedEvent.message.text)#名前
 
             elif user_status[id]==6:
                 text='まず、尾率が５０％以上かどうか教えて下さい。尾率は尾長（尻尾の長さ）を頭胴長（頭から尻尾の付け根までの長さ）で割ると求めることができます。'
@@ -97,22 +96,33 @@ class MessageHandler:
             elif user_status[id]==7:
                 text='鼻上中央に白斑はありますか。下の写真を例にお答えください。'
                 user_status[id]=8
-                user_status[id].append(receivedEvent.message.text)#尾率
+                user_answer[id].append(receivedEvent.message.text)#尾率
 
             elif user_status[id]==8:
                 text='最後に頬と後ろ足の毛色に差はありますか。下の写真を例にお答えください。 (無ければシベリアイタチ/有ればニホンイタチ)'
                 user_status[id]=9
-                user_status[id].append(receivedEvent.message.text)#白斑
+                user_answer[id].append(receivedEvent.message.text)#白斑
 
             elif user_status[id]==9:
                 text='ありがとうございます。判定結果は「〇〇イタチ」でした。今後、この判別方式が有効かどうかを検証するために、今回捕獲されたイタチの写真提供にご協力いただけないでしょうか。全身の写真、顔のアップの写真を提供いただけるとありがたいです。'
                 user_status[id]=10
-                user_status[id].append(receivedEvent.message.text)#毛色の差
+                user_answer[id].append(receivedEvent.message.text)#毛色の差
                 
             elif user_status[id]==10:
                 text='これで質問は終わりです。イタチ判別にご協力いただきありがとうございました。'
                 user_status[id]=0
-                text+=str(user_status[id])
+                # text+=str(user_status[id])
+
+                f = open(id+'.txt', 'w')
+                f.write(str(user_answer[id]))
+                f.close()
+                # アップロードしたいファイル
+                local_filepath = id+'.txt'
+                # send_num[0]+=1
+                アップロード先のファイル名（アップロードしたいファイルと同じ名前でもよい）
+                dropbox_filepath = 'test'+str(user_answer[id])+'.txt'
+                ファイルアップロード
+                client.files_upload(open(local_filepath, "rb").read(), os.path.join(DROPBOX_ROOT, dropbox_filepath))
         # answer=text
         #text='A'
         return text
