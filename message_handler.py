0dad5f62728dedd2d4eb37cb7a6d696ef1cccb7b
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-11-06 16:06:32 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -6,7 +6,6 @@ import dropbox
 # Dropbox(Salmonfs)のアクセストークン
 DROPBOX_ACCESS_TOKEN = 'sl.BnHRHWk2rXzCQdmN0qckxTISBxln8jxuAQw-2gllG4gjeLbgUU1D1gIavCoy0C9qXeLmm4XMKSnI3_jV1OIqJwew8c2N4fQiFwd9YASQEnDLforzgksimjxKSH0Kt9eJt6Doh6i54Ifp'
 # Dropbox(Salmonfs)のリフレッシュトークン
-# DROPBOX_REFRESH_TOKEN = '2fl8VXXfWdsAAAAAAAAAATAUF3oa2VqnRLvWV5E0HFsEgZhnYDIg-UYMjcPh6oIq'
 DROPBOX_REFRESH_TOKEN=os.getenv('CHANNEL_REFRESH_TOKEN')
 # Dropboxのフォルダ
 # Dropboxのルートにこの名前のフォルダを事前に作っておく必要がある
