256c02fb934579ad151bda6dbecc887a7774d06b
tallsalmon <134824946+tallsalmon@users.noreply.github.com>
2023-11-07 22:49:57 +0900

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -24,6 +24,7 @@ user_status={}
 user_answer={}#{userid:[date,place,name,talerate(尾率 はいorいいえ),whitepoint(白斑 はいorいいえ),colordif(毛色の違い はいorいいえ)]}
 itachi_point={}#二ホンイタチの特徴に当てはまる場合+1,シベリアイタチの特徴に当てはまる場合+0　合計が2以上で二ホンイタチ
 user_image={}#写真を上げてくれた枚数（イタチの画像の名前に使用）
+send_mode={}#ユーザーに送るメッセージの種類を決定（1のときテキストメッセージ、2のときテンプレートメッセージ）
 
 class MessageHandler:
     
@@ -70,43 +71,52 @@ class MessageHandler:
         if type=='user':
             if id not in user_status:
                 user_status[id]=0
+                send_mode[id]=1
 
             if user_status[id]==0:
                 text='こんにちは。本日はイタチ判別にご協力いただき誠にありがとうございます。これからいくつか質問をさせていただきますので、できるだけ正確にお答えしていただけるようよろしくお願いいたします。'
                 user_status[id]=1
                 user_answer[id]=[]
                 itachi_point[id]=0
+                send_mode[id]=1
 
             elif user_status[id]==1:
                 text='まずは捕獲した記録を残したいので、以下の３つの質問に答えてください。'
                 user_status[id]=2
+                send_mode[id]=1
 
             elif user_status[id]==2:
                 text='捕獲した日時はいつですか?'
                 user_status[id]=3
+                send_mode[id]=1
 
             elif user_status[id]==3:
                 text='捕獲した場所を教えて下さい。以下のようにお答えください。例：福井県〇〇市〇〇町〇〇番地'
                 user_status[id]=4
                 user_answer[id].append(receivedEvent.message.text)#日時の記録
+                send_mode[id]=1
 
             elif user_status[id]==4:
                 text='捕獲者のお名前を教えて下さい。'
                 user_status[id]=5
                 user_answer[id].append(receivedEvent.message.text)#場所
+                send_mode[id]=1
 
             elif user_status[id]==5:
                 text='次に捕獲したイタチの状態について以下の３つの質問に答えてください。'
                 user_status[id]=6
                 user_answer[id].append(receivedEvent.message.text)#名前
+                send_mode[id]=2
 
             elif user_status[id]==6:
                 text='まず、尾率が５０％以上かどうか教えて下さい。尾率は尾長（尻尾の長さ）を頭胴長（頭から尻尾の付け根までの長さ）で割ると求めることができます。'
                 user_status[id]=7
+                send_mode[id]=2
 
             elif user_status[id]==7:
                 text='鼻上中央に白斑はありますか。下の写真を例にお答えください。'
                 user_status[id]=8
+                send_mode[id]=2
                 user_answer[id].append(receivedEvent.message.text)#尾率
                 if user_answer[id][-1]=='いいえ':
                     itachi_point[id]+=1
@@ -116,6 +126,7 @@ class MessageHandler:
             elif user_status[id]==8:
                 text='最後に頬と後ろ足の毛色に差はありますか。下の写真を例にお答えください。 (無ければシベリアイタチ/有ればニホンイタチ)'
                 user_status[id]=9
+                send_mode[id]=1
                 user_answer[id].append(receivedEvent.message.text)#白斑
                 if user_answer[id][-1]=='いいえ':
                     itachi_point[id]+=1
@@ -123,6 +134,7 @@ class MessageHandler:
                     itachi_point[id]=100
 
             elif user_status[id]==9:
+                send_mode[id]=1
                 user_answer[id].append(receivedEvent.message.text)#毛色の差
                 if user_answer[id][-1]=='はい':
                     itachi_point[id]+=1
@@ -142,6 +154,7 @@ class MessageHandler:
             elif user_status[id]==10:
                 text='これで質問は終わりです。イタチ判別にご協力いただきありがとうございました。'
                 user_status[id]=0
+                send_mode[id]=1
                 # text+=str(user_status[id])
 
                 f = open(id+'.txt', 'w')
@@ -160,7 +173,7 @@ class MessageHandler:
                 client.files_upload(open(local_filepath, "rb").read(), os.path.join(DROPBOX_ROOT, dropbox_filepath))
         # answer=text
         #text='A'
-        return text
+        return send_mode[id],text
 
     def getimage(receivedEvent):
         #userIdとtypeを無理くり取得
